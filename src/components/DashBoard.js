136cadef86b0b156f834bea7b8d6f8b4836aa488
silva <htsilvakt04@gmail.com>
2018-03-26 00:30:34 +0700

    finish dashboard

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,21 +1,57 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-function DashBoard (props) {
+function Answer(props) {
     return (
-        <div>
-            <h1>DashBoard</h1>
-        </div>
+        props.data.map( pool => {
+            return <li key={pool.id}>{pool.question}</li>
+        })
     )
 }
 
+class DashBoard extends React.Component {
+    state = {
+        showAnswered: false
+    }
+    handleToggle = () => {
+        this.setState(() => ({showAnswered: !this.state.showAnswered}))
+    }
+    render () {
+        let {showAnswered} = this.state;
+        let {answered, unAnswered} = this.props;
+        let data = showAnswered === true ? answered : unAnswered;
+
+        return (
+            <div>
+                <div className="dashboard-toggle">
+                    <button onClick={this.handleToggle} style={{textDecoration: showAnswered === true ? 'underline' : null}}>
+                        Answer
+                    </button>
+                    <span>||</span>
+                    <button onClick={this.handleToggle} style={{textDecoration: showAnswered !== true ? 'underline' : null}}>
+                        Unanswered
+                    </button>
+                </div>
+                <ul style={{textAlign: 'center'}} className="dashboard-list">
+                    <Answer data={data}/>
+                </ul>
+            </div>
+        )
+    }
+}
+
 function mapStateToProps({users, pools, authedUser}) {
-    debugger;
+    let answers = users[authedUser].answers;
+
+    let answered = answers.map(id => pools[id]).sort((a,b) => b.timestamp - a.timestamp);
+    let unAnswered = Object.keys(pools).filter(id => !answers.includes(id)).map(id => pools[id]).sort((a,b) => {
+        return b.timestamp - a.timestamp;
+    });
 
     // the idea is to filter all answered && unAnswered question using the state above and pass those answers in to this component (just those answers)
     return {
-        users,
-        pools,
+        answered,
+        unAnswered,
         authedUser
     }
 }
